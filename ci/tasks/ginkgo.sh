beba79000b6ea2c8c692f46e561ed9b6ca628d0a
Gary Preston White <Gary.WhiteJr@emc.com>
2017-07-13 15:24:59 -0400

    made sure to 'cd' to the right place
    Signed-off-by: Thinh Nguyen <thinh.nguyen@emc.com>

--- ci/tasks/ginkgo.sh
+++ ci/tasks/ginkgo.sh
@@ -4,7 +4,7 @@ set -e -x
 
 cp -r go-url-preview $GOPATH/src/github.com/EMC-Dojo/
 
-cd $GOPATH/src/github.com/EMC-Dojo/
+cd $GOPATH/src/github.com/EMC-Dojo/go-url-preview
 
 glide up
-ginkgo -r .
+ginkgo .
